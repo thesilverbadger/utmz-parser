235e5bf0401a9999219b766979b824412e2dd2e3
Tom Wire <tom.wire@poweredbypie.co.uk>
2017-04-04 09:16:54 +0100

    Add a try/catch

--- scripts/utmz-parser.js
+++ scripts/utmz-parser.js
@@ -13,48 +13,54 @@
     //Initialise the dataLayer if one isn't already defined
     window.dataLayer = window.dataLayer || [];
     
-    //get the cookie
-    var cookie = Cookies.get('__utmz');
-    //var cookie = '79104832.1302099445.45.9.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=human%20quality%20management%20eduhub';
+    try {
+        //get the cookie
+        var cookie = Cookies.get('__utmz');
 
-    if(cookie){
-        //first, split the cookie value by . to separate the initial 4 values
-        var z = cookie.split('.');
+        //Test values for the cookie
+        //var cookie = '79104832.1302099445.45.9.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=human%20quality%20management%20eduhub';
 
-        window.dataLayer.push({name: 'domain', value: z[0]});
-        window.dataLayer.push({name: 'timestamp', value: z[1]});
-        window.dataLayer.push({name: 'visits', value: z[2]});
-        window.dataLayer.push({name: 'sources', value: z[3]});
+        if(cookie){
+            //first, split the cookie value by . to separate the initial 4 values
+            var z = cookie.split('.');
 
-        if (z.length >= 4) {
-            //now split the latter part of the cookie
-            var y = z[4].split('|');
-            for (i = 0; i < y.length; i++) {
+            window.dataLayer.push({name: 'domain', value: z[0]});
+            window.dataLayer.push({name: 'timestamp', value: z[1]});
+            window.dataLayer.push({name: 'visits', value: z[2]});
+            window.dataLayer.push({name: 'sources', value: z[3]});
 
-                //get the value for each key=value pair
-                var pair = y[i].split("=");
+            if (z.length >= 4) {
+                //now split the latter part of the cookie
+                var y = z[4].split('|');
+                for (i = 0; i < y.length; i++) {
 
-                switch(pair[0]){
+                    //get the value for each key=value pair
+                    var pair = y[i].split("=");
 
-                    case "utmcsr":
-                    window.dataLayer.push({name: 'campaign', value: pair[1]});
-                    break;
+                    switch(pair[0]){
 
-                    case "utmccn":
-                    window.dataLayer.push({name: 'source', value: pair[1]});
-                    break;
+                        case "utmcsr":
+                        window.dataLayer.push({name: 'campaign', value: pair[1]});
+                        break;
 
-                    case "utmcmd":
-                    window.dataLayer.push({name: 'medium', value: pair[1]});
-                    break;
+                        case "utmccn":
+                        window.dataLayer.push({name: 'source', value: pair[1]});
+                        break;
 
-                    case "utmctr":
-                    window.dataLayer.push({name: 'keyword', value: pair[1]});
-                    break;
+                        case "utmcmd":
+                        window.dataLayer.push({name: 'medium', value: pair[1]});
+                        break;
+
+                        case "utmctr":
+                        window.dataLayer.push({name: 'keyword', value: pair[1]});
+                        break;
+                    }
                 }
             }
-        }
 
-        console.log(window.dataLayer);
+            console.log(window.dataLayer);
+        }
+    } catch(ex){
+        console.log(ex);
     }
 }());
